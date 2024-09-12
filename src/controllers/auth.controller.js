637099e7f84f85521f7b7ab3ee45c7a59bce7db1
Enrique <enriquesamudio1999@gmail.com>
2024-09-12 16:19:38 -0400

    Some updates in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,9 +2,7 @@ import User from "../models/user.js";
 import { loginSchema, registerSchema, updatePasswordSchema, updateSchema } from "../validations/user.validation.js";
 import { generateRandomToken, generateToken, validateObjectId } from "../helpers/utilities.js";
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import { forgetPasswordEmail } from '../helpers/email.js';
-import { SECRET_WORD } from "../config/env.js";
  
 const loginUser = async (req, res) => {
   const { error, value } = loginSchema.validate(req.body);
@@ -37,34 +35,27 @@ const loginUser = async (req, res) => {
       });
     }
 
-    // Refresh Token
-    const refreshToken = generateToken({
-      _id: userExists._id,
-      name: `${userExists.firstName} ${userExists.lastName}`,
-      email: userExists.email,
-      bio: userExists.bio
-    });
-
-    await User.findByIdAndUpdate(userExists._id, { refreshToken }, { new: true });
-
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      maxAge: 72 * 60 * 60 * 1000
-    });
-
     userExists.password = undefined;
 
     // JWT
     const accessToken = generateToken({
       _id: userExists._id,
       name: `${userExists.firstName} ${userExists.lastName}`,
+      username: userExists.username,
       email: userExists.email,
       bio: userExists.bio,
     });
 
     return res.status(200).json({
       success: true,
-      token: accessToken
+      token: accessToken, 
+      user: {
+        id: userExists._id,
+        name: `${userExists.firstName} ${userExists.lastName}`,
+        username: userExists.username,
+        email: userExists.email,
+        bio: userExists.bio
+      }
     });
   } catch (error) {
     console.log(error);
@@ -113,34 +104,27 @@ const registerUser = async (req, res) => {
 
     const result = await user.save();
 
-    // Refresh Token
-    const refreshToken = generateToken({
-      _id: result._id,
-      name: `${result.firstName} ${result.lastName}`,
-      email: result.email,
-      bio: result.bio
-    });
-
-    await User.findByIdAndUpdate(result._id, { refreshToken }, { new: true });
-
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      maxAge: 72 * 60 * 60 * 1000
-    });
-
     result.password = undefined;
 
     // JWT
     const accessToken = generateToken({
       _id: result._id,
       name: `${result.firstName} ${result.lastName}`,
+      username: result.username,
       email: result.email,
       bio: result.bio
     });
 
     return res.status(200).json({
       success: true,
-      token: accessToken
+      token: accessToken,
+      user: {
+        id: result._id,
+        name: `${result.firstName} ${result.lastName}`,
+        username: result.username,
+        email: result.email,
+        bio: result.bio
+      }
     });
   } catch (error) {
     console.log(error);
@@ -148,53 +132,29 @@ const registerUser = async (req, res) => {
 }
 
 const refreshUserToken = async (req, res) => {
-  const cookies = req.cookies;
 
-  if (!cookies?.refreshToken) {
-    return res.status(404).json({
-      success: false,
-      error: 'No refresh token.'
-    });
-  }
+  const { _id, name, username, email, bio } = req.user;
 
-  const refreshToken = cookies?.refreshToken;
-  
-  const userWithToken = await User.findOne({ refreshToken });
-
-  if (!userWithToken) {
-    return res.status(404).json({
-      success: false,
-      error: 'Token not found.'
-    });
-  }
- 
   try {
-    
-    const decodedData = jwt.verify(refreshToken, SECRET_WORD);
-
-    if (!userWithToken._id.equals(decodedData._id)) {
-      return res.status(401).json({
-        error: 'Something wrong.'
-      });
-    }
-
-    const updatedRefreshToken = generateToken({
-      _id: userWithToken._id,
-      name: `${userWithToken.firstName} ${userWithToken.lastName}`,
-      email: userWithToken.email,
-      bio: userWithToken.bio
-    });
-
-    await User.findByIdAndUpdate(userWithToken._id, { refreshToken: updatedRefreshToken }, { new: true });
-
-    res.cookie('refreshToken', updatedRefreshToken, {
-      httpOnly: true,
-      maxAge: 72 * 60 * 60 * 1000
+    // JWT
+    const accessToken = generateToken({
+      _id,
+      name,
+      username,
+      email,
+      bio
     });
 
     return res.json({
       success: true,
-      token: updatedRefreshToken
+      token: accessToken,
+      user: {
+        id: _id,
+        name,
+        username,
+        email,
+        bio
+      }
     });
 
   } catch (error) {
@@ -203,45 +163,6 @@ const refreshUserToken = async (req, res) => {
     return res.status(401).json({
       error: 'Invalid token.'
     });
-  }
-}
-
-const logoutUser = async (req, res) => {
-  const cookies = req.cookies;
-
-  if (!cookies?.refreshToken) {
-    return res.status(404).json({
-      success: false,
-      error: 'No refresh token.'
-    });
-  }
-
-  const refreshToken = cookies?.refreshToken;
-
-  const userWithToken = await User.findOne({ refreshToken });
-
-  if (!userWithToken) {
-    res.clearCookie('refreshToken', {
-      httpOnly: true,
-      secure: true
-    });
-
-    return res.sendStatus(204);
-  }
-
-  try {
-
-    await User.findOneAndUpdate({ refreshToken }, { refreshToken: "" });
-
-    res.clearCookie('refreshToken', {
-      httpOnly: true,
-      secure: true
-    });
-
-    return res.sendStatus(204);
-    
-  } catch (error) {
-    console.log(error);
   }
 }
 
@@ -484,7 +405,6 @@ export {
   loginUser,
   registerUser,
   refreshUserToken,
-  logoutUser,
   updateUser,
   updateUserPassword,
   deleteUser,
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,21 +3,19 @@ import {
   createResetPasswordToken,
   deleteUser,
   loginUser, 
-  logoutUser, 
   refreshUserToken, 
   registerUser, 
   resetUserPassword, 
   updateUser,
   updateUserPassword
 } from '../controllers/auth.controller.js';
-import { verifyTokenAndUser } from '../middlewares/auth.js';
+import { verifyToken, verifyTokenAndUser } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.post('/login', loginUser); 
 router.post('/register', registerUser); 
-router.get('/refresh', refreshUserToken); 
-router.get('/logout', logoutUser); 
+router.get('/refresh', verifyToken, refreshUserToken); 
 router.post('/update/:userId', verifyTokenAndUser, updateUser); 
 router.post('/update-password/:userId', verifyTokenAndUser, updateUserPassword); 
 router.delete('/delete/:userId', verifyTokenAndUser, deleteUser); 
