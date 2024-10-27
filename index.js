d01a2010cb4c71372df7855e08d2397255dd9e2b
Enrique <enriquesamudio1999@gmail.com>
2024-10-27 13:37:08 -0300

    Changes in vercel.json

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import app from './src/app.js';
 import dbConnection from './src/config/db.js';
-import { PORT, BASE_API_URL } from './src/config/env.js';
+import { BACKEND_URL, PORT, BASE_API_URL } from './src/config/env.js';
 
 function main() {
   // Connect to DB
   dbConnection(); 
   // Configure port and run the server 
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    console.log(`API Version http://localhost:${PORT}/${BASE_API_URL}`);
+    console.log(`Server running on ${BACKEND_URL}:${PORT}`);
+    console.log(`API Version ${BACKEND_URL}:${PORT}/${BASE_API_URL}`);
   }); 
 }
 
